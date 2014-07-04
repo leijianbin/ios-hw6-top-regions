7a91191af46c3540e6f56d0ebda3be320ca1a019
Ray <leijianljb@gmail.com>
2014-07-04 09:45:49 -0700

    Add Flickr Fetcher

---
+++ TopRegions/Flickr Fetcher/FlickrAPIKey.h
@@ -0,0 +1,18 @@
+//
+//  FlickrAPIKey.h
+//
+//  Created for Stanford CS193p Fall 2013.
+//  Copyright 2013 Stanford University. All rights reserved.
+//
+//  Get your own key!
+//  No Flickr fetches will work without the API Key!
+//
+
+#define FlickrAPIKey @"8c1533c7aa18db2ff09ea9149b942ffe"
+
+
+//Top Places
+//Key:
+//8c1533c7aa18db2ff09ea9149b942ffe
+//Secret:
+//47950e59a776b61a
